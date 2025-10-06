f7368f64137d5e187ac61e2148f1edb2d5391bd8
Johnny Hyytiäinen <Johnny.Hyytiainen@gmail.com>
2025-10-06 20:27:09 +0200

    Start task 1: setup + notes.md

---
+++ lab_1/task_1_lab.py
@@ -0,0 +1,8 @@
+# kommentarer på svenska, kod på engelska.
+# Steg 1 – Importer.
+# Varför: jag vill räkna bokstäver enkelt (Counter) och (senare) kunna plotta (matplotlib).
+
+# Steg 2 - Filväg.
+# Varför: Vi säger var filen finns.
+path = "dna_raw.txt"
+print("Path:", path)
